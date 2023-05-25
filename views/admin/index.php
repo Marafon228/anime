eb2ac0a8a2c6130c3be63b008ad33649c75ec298
IvvanFromDreams <denisoff.ivvan@mail.ru>
2023-05-26 02:38:45 +0300

    add admin panel

---
+++ views/admin/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Admin';
+?>
+<div class="site-index">
+    <div class="body-content">
+        <ul>
+            <li><a href="/web/users">Управление клиентами</a></li>
+            <li><a href="/web/orders">Управление заказами</a></li>
+            <li><a href="/web/category">Управление категориями</a></li>
+            <li><a href="/web/products">Управление продуктами</a></li>
+            <li><a href="/web/talismans">Управление талисманами</a></li>
+        </ul>
+
+    </div>
+</div>
